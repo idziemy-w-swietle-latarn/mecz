bac8b6a7c9e71b60e5a2c26f055fc16598aa2ed5
Ubuntu <ubuntu@ip-172-31-93-235.ec2.internal>
2023-04-05 21:29:49 +0000

    added run_deploy.sh

---
+++ run_deploy.sh
@@ -0,0 +1,2 @@
+source /home/ubuntu/mambaforge/bin/activate /home/ubuntu/mambaforge/envs/socials && conda activate socials && python /home/ubuntu/programowanie/mecz/deploy_fromFixtures.py >> /tmp/deploy_log.log
+
