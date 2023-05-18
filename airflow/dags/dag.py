55e52e3934901c416a526246b83d2a78410f414f
idziemy_w_swietle_latarn <idziemy_w_swetle_latarn@protonmail.com>
2023-05-18 20:37:23 +0200

    airflow

---
+++ airflow/dags/dag.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator 
+from deploy_fromFixtures import main
+from datetime import datetime
+
+dag = DAG(dag_id='deploy_fromFixtures',
+          schedule_interval="0 9 * * *",
+          start_date=datetime(2023, 5, 18, 15, 50)
+          )
+
+deploy_fromFixtures = PythonOperator(python_callable=main,
+                                     dag=dag,
+                                     task_id='deploy_fromFixtures')
+
+
+deploy_fromFixtures
--- deploy_fromFixtures.py
+++ deploy_fromFixtures.py
@@ -12,44 +12,44 @@ headers = {
 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36',
 }
 
-f = Fixtures()
-checked_date = date.today()
-checked_link = f.link(checked_date)
-r = requests.get(checked_link, headers=headers)
-soup = BeautifulSoup(r.text, 'html.parser')
-day_dict = defaultdict(list)
-
-fixtures = f.matches_by_competitions(soup)
-for key, value in fixtures.items():
-    submatches = []
-    title = value[0]
-    submatches.append(15*'*' + '   ' + title + '  ' + 15*'*')
-    key = key.split('/')[4]
-    key = leagues.get(key)
-    
-    for game in value[1]:
-        submatches.append(game_dict_parser(f.single_match(game)))
-
-    if key:
-        day_dict[key].extend(submatches)
-        
-threads = {}   
-
 order_queue = ['Europa Wschodnia', 'Europa Zachodnia', 'Europa Południowa', 'Wielka Brytania', 
-               'Rozgrywki Polskiego Związku Piłki Nożnej', 'Ekstraklasa']
+               'Rozgrywki Polskiego Związku Piłki Nożnej', 'Ekstraklasa', 'Liga Konferencji Europy', 'Liga Europy', 'Liga Mistrzów']
 
-for league_submition in order_queue:
-    value = day_dict.get(league_submition)
-    key = league_submition
-    if value:
-        thread = subreddit.submit(title_text(key), selftext = body_text(value), discussion_type='CHAT')
-        threads[key] = thread.id
+def main():
+    f = Fixtures()
+    checked_date = date.today()
+    checked_link = f.fixtures_link(checked_date)
+    r = requests.get(checked_link, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    day_dict = defaultdict(list)
 
-# for key, value in day_dict.items():
-#     thread = subreddit.submit(title_text(key), selftext = body_text(value), discussion_type='CHAT')
-#     threads[key] = thread.id
-    
-with open('data.json', 'w', encoding='utf-8') as f:
-    json.dump(threads, f, ensure_ascii=False, indent=4)
+    fixtures = f.matches_by_competitions(soup)
+    for key, value in fixtures.items():
+        submatches = []
+        title = value[0]
+        submatches.append(15*'*' + '   ' + title + '  ' + 15*'*')
+        key = key.split('/')[4]
+        key = leagues.get(key)
+        
+        for game in value[1]:
+            submatches.append(game_dict_parser(f.single_match(game)))
 
-#
+        if key:
+            day_dict[key].extend(submatches)
+        
+
+    threads = {}   
+
+    for league_submition in order_queue:
+        value = day_dict.get(league_submition)
+        key = league_submition
+        if value:
+            thread = subreddit.submit(title_text(key), selftext = body_text(value), discussion_type='CHAT')
+            threads[key] = thread.id
+
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(threads, f, ensure_ascii=False, indent=4)
+
+
+if __name__ == '__main__':
+    main()
