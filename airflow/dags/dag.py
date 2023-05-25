8d99f9d4017e3b5112561f0e7e2864a910c28124
idziemy_w_swietle_latarn <idziemy_w_swetle_latarn@protonmail.com>
2023-05-25 20:41:16 +0200

    data transfer

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -1,10 +1,16 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator 
-from ...deploy_fromFixtures import main
+import sys
+# directory reach
+directory = sys.path.path(__file__).abspath() 
+# setting path
+sys.path.append(directory.parent.parent) 
+# importing
+from deploy_fromFixtures import main
 from datetime import datetime
 
 dag = DAG(dag_id='deploy_fromFixtures',
-          schedule_interval="0 9 * * *",
+          schedule_interval="0 4 * * *",
           start_date=datetime(2023, 5, 18, 15, 50)
           )
 
