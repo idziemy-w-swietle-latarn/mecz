5ac02d14ccc26596174f308a091cb0a6797fd59d
idziemy_w_swietle_latarn <idziemy_w_swetle_latarn@protonmail.com>
2023-05-25 19:53:01 +0200

    data transfer

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator 
-from deploy_fromFixtures import main
+from ...deploy_fromFixtures import main
 from datetime import datetime
 
 dag = DAG(dag_id='deploy_fromFixtures',
